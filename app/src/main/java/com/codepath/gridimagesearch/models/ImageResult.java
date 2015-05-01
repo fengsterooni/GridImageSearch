562e1a73dbcbf3c4bb6c07645dcf14345886c2c6
Feng Guo <fengsterooni@gmail.com>
2015-04-30 17:59:38 -0700

    initial version

---
+++ app/src/main/java/com/codepath/gridimagesearch/models/ImageResult.java
@@ -0,0 +1,41 @@
+package com.codepath.gridimagesearch.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ImageResult implements Serializable {
+    private static final long serialVersionUID = -4677141361493269206L;
+
+    public String fullUrl;
+    public String thumbUrl;
+    public String title;
+    public int height;
+    public int width;
+
+    public ImageResult(JSONObject json) {
+        try {
+            this.fullUrl = json.getString("url");
+            this.thumbUrl = json.getString("tbUrl");
+            this.title = json.getString("title");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ArrayList<ImageResult> fromJSONArray(JSONArray array) {
+        ArrayList<ImageResult> results = new ArrayList<ImageResult>();
+
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                results.add(new ImageResult(array.getJSONObject(i)));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+}
